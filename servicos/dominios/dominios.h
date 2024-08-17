234f6482e5e9c6a4054df0181f4479261372bd99
Tiago nunes <117403385+Tiagonuness@users.noreply.github.com>
2024-08-17 00:45:53 -0300

    inicio
    corpo inicial do projeto

---
+++ servicos/dominios/dominios.h
@@ -0,0 +1,138 @@
+#ifndef DOMINIOS_H
+#define DOMINIOS_H
+#include <iostream>
+#include <stdexcept>
+#include <regex>
+#include <map>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class Dominio {
+public:
+    virtual ~Dominio() = default;
+    virtual void validar(string valor) = 0;
+    virtual void setValor(string valor) = 0;
+    virtual string getValor() = 0;
+};
+
+class Dinheiro : public Dominio {
+private:
+    float dinheiroValue;
+    static constexpr float LIMITE_INF = 0.00;
+    static constexpr float LIMITE_SUP = 1000000.00;
+public:
+    void validar(string valor) override;
+    void setValor(string valor) override;
+    string getValor() override {
+        ostringstream aux;
+        aux << fixed << setprecision(2) << dinheiroValue;
+        return aux.str();};
+};
+
+class Percentual : public Dominio {
+private:
+    int percentualValue;
+    static constexpr unsigned short LIMITE_INF = 0;
+    static constexpr unsigned short LIMITE_SUP = 100;
+public:
+    void validar(string valor) override;
+    void setValor(string valor) override;
+    string getValor() override {return to_string(percentualValue);};
+};
+
+class CodigoDePagamento : public Dominio {
+private:
+        string codigoDePagamentoValue;
+        static constexpr unsigned short TAMANHO = 8;
+public:
+        void validar(string valor) override;
+        void setValor(string valor) override;
+        string getValor() override {return codigoDePagamentoValue;};
+};
+
+class CodigoDeTitulo : public Dominio {
+private:
+        string codigoDeTituloValue;
+        static constexpr unsigned short TAMANHO = 11;
+        static const char* siglasCodigosDeTitulo[];
+public:
+        void validar(string valor) override;
+        void setValor(string valor) override;
+        string getValor() override {return codigoDeTituloValue;};
+};
+
+class Cpf : public Dominio {
+private:
+        string cpfValue;
+        static constexpr unsigned short TAMANHO = 11    ;
+public:
+        void validar(string valor) override;
+        void setValor(string valor) override;
+        string getValor() override {return cpfValue;};
+};
+
+class Data : public Dominio {
+private:
+        string dataValue;
+        static const map<string, int>& getLimites() {
+            static const map<string, int> limites = {
+                {"diaMin", 1},
+                {"diaMax", 31},
+                {"mesMin", 1},
+                {"mesMax", 12},
+                {"anoMin", 2000},
+                {"anoMax", 2100},
+                {"diaMaxFevereiro", 28}
+            };
+            return limites;
+        }
+public:
+        void validar(string valor) override;
+        void setValor(string valor) override;
+        string getValor() override {return dataValue;};
+};
+
+class Estado : public Dominio {
+private:
+    string estadoValue;
+public:
+    void validar(string valor) override;
+    void setValor(string valor) override;
+    string getValor() override {return estadoValue;};
+};
+
+class Nome : public Dominio {
+private:
+    string nomeValue;
+public:
+    void validar(string valor) override;
+    void setValor(string valor) override;
+    string getValor() override {return nomeValue;};
+};
+
+class Senha : public Dominio {
+private:
+    string senhaValue;
+    static constexpr unsigned short TAMANHO = 6;
+public:
+    void validar(string valor) override;
+    void setValor(string valor) override;
+    string getValor() override {return senhaValue;};
+};
+
+class Setor : public Dominio {
+private:
+    string setorValue;
+    static const char* setores[];
+public:
+    void validar(string valor) override;
+    void setValor(string valor) override;
+    string getValor() override {return setorValue;};
+};
+
+#endif // DOMINIOS_H
---
+++ servicos/entidades/entidades.h
@@ -0,0 +1,75 @@
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+#include "dominios.h"
+
+class Entidade {
+public:
+    virtual ~Entidade() = default;
+    virtual void setId() = 0;
+    virtual string getId() = 0;
+};
+
+class Pagamento : public Entidade {
+private:
+    CodigoDePagamento codigo;
+    Data data;
+    Percentual percentual;
+    Estado estado;
+    string Id;
+public:
+    void setId() override;
+    string getId() override {return Id;};
+    void setCodigoPagamento(const CodigoDePagamento&);
+    CodigoDePagamento getCodigoPagamento() const {return codigo;};
+    void setData(const Data&);
+    Data getData() const {return data;};
+    void setPercentual(const Percentual&);
+    Percentual getPercentual() const {return percentual;};
+    void setEstado(const Estado&);
+    Estado getEstado() const {return estado;};
+};
+
+class Titulo : public Entidade {
+private:
+    CodigoDeTitulo codigo;
+    Nome emissor;
+    Setor setor;
+    Data emissao;
+    Data vencimento;
+    Dinheiro valor;
+    string Id;
+public:
+    void setId() override;
+    string getId() override {return Id;};
+    void setCodigoTitulo(const CodigoDeTitulo&);
+    CodigoDeTitulo getCodigoTitulo() const {return codigo;};
+    void setNome(const Nome&);
+    Nome getNome() const {return emissor;};
+    void setSetor(const Setor&);
+    Setor getSetor() const {return setor;};
+    void setDataEmissao(const Data&);
+    Data getDataEmissao() const {return emissao;};
+    void setDataVencimento(const Data&);
+    Data getDataVencimento() const {return vencimento;};
+    void setDinheiro(const Dinheiro&);
+    Dinheiro getDinheiro() const {return valor;};
+};
+
+class Conta : public Entidade {
+private:
+    Cpf cpf;
+    Nome nome;
+    Senha senha;
+    string Id;
+public:
+    void setId() override;
+    string getId() override {return Id;};
+    void setCpf(const Cpf&);
+    Cpf getCpf() const {return cpf;};
+    void setNome(const Nome&);
+    Nome getNome() const {return nome;};
+    void setSenha(const Senha&);
+    Senha getSenha() const {return senha;};
+};
+
+#endif // ENTIDADES_H
